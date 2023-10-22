e0d848d2248ed109e77cb3750f54c19fcdbe58f5
Luca-it15 <75178765+Luca-it15@users.noreply.github.com>
2023-10-22 15:23:49 +0200

    Add files via upload

---
+++ turbidimetro/afterStartup.sh
@@ -0,0 +1,11 @@
+[ -z $BASH ] && { exec bash "$0" "$@" || exit; }
+user=$(awk -F'=' '/\[general\]/{flag=1; next} /\[.*\]/{flag=0} flag && $1=="user"{print $2}' ./Desktop/progetto/config.ini)
+#!/bin/bash
+
+# include utilities scripts in same directory
+dir=/home/$user/wittypi
+. $dir/utilities.sh
+. $dir/gpio-util.sh
+echo $(get_output_voltage) > /home/$user/Desktop/progetto/voltage.txt
+cd /home/$user/Desktop/progetto/
+sudo ./startTurbi.sh 
