7fcc53b3c5fe35144f5e7856333eae869283acf7
Luca-it15 <75178765+Luca-it15@users.noreply.github.com>
2023-12-16 10:39:51 +0100

    Add files via upload

---
+++ turbidimetro/afterStartup.sh
@@ -0,0 +1,25 @@
+[ -z $BASH ] && { exec bash "$0" "$@" || exit; }
+#user=$(awk -F'=' '/\[general\]/{flag=1; next} /\[.*\]/{flag=0} flag && $1=="user"{print $2}' ./Desktop/progetto/config.ini)
+user=$(awk -F'=' '/\[general\]/{flag=1; next} /\[.*\]/{flag=0} flag && $1=="user"{print $2}' config.ini)
+#!/bin/bash
+
+synchronize_with_network_time()
+{
+  if $(has_internet) ; then
+    log '  Internet detected, apply network time to system and Witty Pi...'
+    net_to_system
+    system_to_rtc
+  else
+    log '  Internet not accessible, skip time synchronization.'
+  fi
+}
+
+
+# include utilities scripts in same directory
+dir=/home/$user/wittypi
+. $dir/utilities.sh
+. $dir/gpio-util.sh
+echo $(get_input_voltage) > /home/$user/Desktop/progetto/voltage.txt
+cd /home/$user/Desktop/progetto/
+./startTurbi.sh 
+echo $(synchronize_with_network_time)
