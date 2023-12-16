6f497f9a2241216d0b884d45ccb96061395cfc04
Luca-it15 <75178765+Luca-it15@users.noreply.github.com>
2023-12-16 11:10:41 +0100

    Add files via upload

---
+++ server/php/config.php
@@ -0,0 +1,7 @@
+<?php
+	// File system constants
+	define ("DIR_BASE", __DIR__ . '/');
+	define ("DIR_UTIL", DIR_BASE . 'util/');
+	define ("DIR_LAYOUT", DIR_BASE . 'layout/');
+	define ("DIR_AJAX_UTIL", DIR_BASE . 'ajax/');
+?>
