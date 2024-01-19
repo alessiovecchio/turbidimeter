39c81775f1054286d5b4492d463b56aa536f3547
Gianlu0808 <gianluca.schiano@yahoo.it>
2024-01-19 19:01:36 +0100

    Aggiunto Raspi/venv

---
+++ turbidimeter_python/Raspi/venv/lib/python3.11/site-packages/python_tsl2591/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for python-tsl2591."""
+
+from __future__ import absolute_import
+from .__version__ import __version__
+
+from python_tsl2591.sensor import *
