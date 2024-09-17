5fd3fa3e7d8d22155610416250d2193aa89e13ec
Maurizio Palmieri <maupalm88@gmail.com>
2024-09-17 18:04:22 +0200

    Delete old server directory

--- server/php/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// File system constants
-	define ("DIR_BASE", __DIR__ . '/');
-	define ("DIR_UTIL", DIR_BASE . 'util/');
-	define ("DIR_LAYOUT", DIR_BASE . 'layout/');
-	define ("DIR_AJAX_UTIL", DIR_BASE . 'ajax/');
-?>
--- server/php/util/dbConfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$host = 'localhost:3306';
-	$Username = 'root';
-	$Password = 'luca1';
-	$name ='turbidimeterdb';
-?>
